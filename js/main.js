47414ce273805b983eddc415201eaeb925b636f6
Jaymiller2002 <jay.miller02@icloud.com>
2024-03-28 15:26:07 -0700

    fixed some code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const displayedImage = document.querySelector('.displayed-img');
+const displayedImage = document.querySelector('.gallery-item');
 const thumbBar = document.querySelector('.thumb-bar');
 
 const btn = document.querySelector('button');
