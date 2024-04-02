5c39d8c8d2685253023e50532f4d612ee22cbfa4
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-02 16:35:21 -0700

    changed js

--- js/main.js
+++ js/main.js
@@ -5,23 +5,9 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const images = [
-    'C:\Users\jaymi\OneDrive\Documents\imageGallery\img\space1.jpg',
-    'C:\Users\jaymi\OneDrive\Documents\imageGallery\img\space2.jpg',
-    'C:\Users\jaymi\OneDrive\Documents\imageGallery\img\space3.jpg',
-    'C:\Users\jaymi\OneDrive\Documents\imageGallery\img\space4.jpg',
-    'C:\Users\jaymi\OneDrive\Documents\imageGallery\img\space5.jpg'
-            ];
-         
-            const container = document.getElementById('displayed-img');
-         
-            for (let i = 0; i < images.length; i++) {
-                const img = document.createElement('img');
-                img.src = images[i];
-                container.appendChild(img);
-            }
+const src = ["space1.jpg","space2.jpg","space3.jpg","space4.jpg","space5.jpg"];
 /* Declaring the alternative text for each image file */
-const alt = ["space 1", "space 2", "space 3", "space 4", "space 5"];
+const alt = ["SPACE"];
 /* Looping through images */
 
 const newImage = document.createElement('img');
