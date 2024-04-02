96eb470320909b668421c431bfebf3d7484a35ce
Jaymiller2002 <jay.miller02@icloud.com>
2024-04-02 16:41:56 -0700

    had to restart and start again but ill be done soon

--- js/main.js
+++ js/main.js
@@ -5,14 +5,48 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const src = ["space1.jpg","space2.jpg","space3.jpg","space4.jpg","space5.jpg"];
-/* Declaring the alternative text for each image file */
-const alt = ["SPACE"];
-/* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+const imgSrcAltText = [
+    {
+      url:
+        "C:\Users\jaymi\OneDrive\Documents\imageGallery\img\space1.jpg",
+      alt: "Closeup of a human eye"
+    },
+    {
+      url:
+        "C:\Users\jaymi\OneDrive\Documents\imageGallery\img\space2.jpg",
+      alt: "An obscure marble stone"
+    },
+    {
+      url:
+        "C:\Users\jaymi\OneDrive\Documents\imageGallery\img\space3.jpg",
+      alt: "Bunch of brunfelsia pauciflora"
+    },
+    {
+      url:
+        "C:\Users\jaymi\OneDrive\Documents\imageGallery\img\space4.jpg",
+      alt: "An egyptian ancient wall-painting demonstrating human behavior"
+    },
+    {
+      url:
+        "C:\Users\jaymi\OneDrive\Documents\imageGallery\img\space5.jpg",
+      alt: "A butterfly sitting over a large leaf"
+    }
+  ];
+  
+  /* Looping through images */
+  
+  for (let imgItem of imgSrcAltText) {
+    const newImage = document.createElement("img");
+  
+    newImage.setAttribute("src", imgItem.url);
+    newImage.setAttribute("alt", imgItem.alt);
+  
+    thumbBar.appendChild(newImage);
+  
+    newImage.addEventListener("click", (e) => {
+      displayedImage.src = e.target.src;
+      displayedImage.alt = e.target.alt;
+    });
+  }
 
 /* Wiring up the Darken/Lighten button */
